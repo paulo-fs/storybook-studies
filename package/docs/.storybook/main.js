0a9f684f3c815add9625bde29686fa866ffd893c
paulo-fs <jr.paulo87@gmail.com>
2023-05-29 15:29:48 -0300

    chore: lib downgrade

--- package/docs/.storybook/main.js
+++ package/docs/.storybook/main.js
@@ -1,33 +1,26 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: [
+module.exports = {
+  "stories": [
     "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/stories/**/*.stories.tsx"
   ],
-  addons: [
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    "@storybook/addon-a11y"
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-    core: {
-      "builder": "@storybook/builder-vite"
-    }
-  },
-  docs: {
-    autodocs: "tag",
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
   "features": {
     "storyStoreV7": true
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/storybook-studies'
+      config.base = '/storybook-studies/'
     }
+
     return config
   }
-};
-export default config;
+}
