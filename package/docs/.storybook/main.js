9a5b991242d1602c421b80bd0428dd9998ddae4e
paulo-fs <jr.paulo87@gmail.com>
2023-05-29 15:07:53 -0300

    fix: add subpath to storybook on production

--- package/docs/.storybook/main.js
+++ package/docs/.storybook/main.js
@@ -8,13 +8,26 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
+    core: {
+      "builder": "@storybook/builder-vite"
+    }
   },
   docs: {
     autodocs: "tag",
   },
+  "features": {
+    "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/storybook-studies'
+    }
+    return config
+  }
 };
 export default config;
