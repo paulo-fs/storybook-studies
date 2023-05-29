c44a4f48c6f80e6c1d48d846bbd8186e29911214
paulo-fs <jr.paulo87@gmail.com>
2023-05-29 09:49:26 -0300

    doc: add button component and its documentation

---
+++ package/react/src/components/Button.tsx
@@ -0,0 +1,94 @@
+import { ComponentProps, ElementType } from "react";
+import { styled } from "../styles";
+
+export const Button = styled('button', {
+   all: 'unset',
+   borderRadius: '$sm',
+   fontSize: '$sm',
+   fontWeight: '$medium',
+   fontFamily: '$default',
+   textAlign: 'center',
+   minWidth: 120,
+
+   display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'center',
+   gap: '$2',
+
+   cursor: 'pointer',
+
+   '&:disabled': {
+      cursor: 'not-allowed'
+   },
+
+   svg: {
+      width: 16,
+      height: 16,
+   },
+
+   variants: {
+      variant: {
+         primary: {
+            color: '$white',
+            backgroundColor: '$ignite500',
+
+            '&:not(:disabled):hover': {
+               backgroundColor: '$ignite300'
+            },
+
+            '&:disabled': {
+               backgroundColor: '$gray200',
+            }
+         },
+
+         secondary: {
+            color: '$ignite300',
+            border: '2px solid $ignite500',
+
+            '&:not(:disabled):hover': {
+               backgroundColor: '$ignite500',
+               color: '$white'
+            },
+
+            '&:disabled': {
+               color: '$gray200',
+               borderColor: '$gray200'
+            }
+         },
+
+         tertiary: {
+            color: '$gray100',
+
+            '&:not(:disabled):hover': {
+               color: '$white'
+            },
+
+            '&:disabled': {
+               color: '$gray200',
+               borderColor: '$gray600'
+            }
+         }
+      },
+
+      size: {
+         sm: {
+            padding: '0 $4',
+            height: 38
+         },
+
+         md: {
+            padding: '0 $4',
+            height: 46
+         }
+      }
+   },
+
+   defaultVariants: {
+      variant: 'primary',
+      size: 'md'
+   }
+})
+
+export interface ButtonProps extends ComponentProps<typeof Button> {
+   as?: ElementType
+}
